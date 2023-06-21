11616445983bba21f8612b98b13abf1661456e15
PraveenaKANINI <praveena.kanini@gmail.com>
2023-06-21 12:03:11 +0530

    LeaveAPI Model

---
+++ LeaveAPI/Models/Leave.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveAPI.Models
+{
+    public class Leave { 
+
+
+        [Key]
+        public int  LeaveId { get; set; }
+        public string? LeaveType { get; set; }
+        [Required(ErrorMessage = "Employee Id cannot be empty")]
+        public string? EmpId { get; set; }
+        public string? ManagerId { get; set;}
+        [MinLength(10, ErrorMessage = "Reason should be Detailed")]
+        public string? Reason { get; set;}
+        [Required(ErrorMessage ="Start date should be given")]
+        public DateTime? StartDate { get; set; }
+        [Required(ErrorMessage = "End date should be given")]
+
+        public DateTime? EndDate { get; set;}
+        public string? LeaveStatus { get; set; } 
+
+
+    }
+}
